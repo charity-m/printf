e085dbc33253b513bfca4b9c53addd9975254103
Waerumuthoga <waerumuthoga@gmail.com>
2023-09-19 15:52:18 -0700

    task 0 changes

--- printf_0.c
+++ printf_0.c
@@ -28,7 +28,7 @@ int _printf(const char *format, ...)
 		{
 			str_count = putss(va_arg(args, char*));
 			i++;
-			count += (str_count - 1);
+			count += str_count;
 		}
 		else if (format[i + 1] == '%')
 		{
