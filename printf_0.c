ea92e62063f615bd58e4eaf1e983cdbfe88a0279
Waerumuthoga <waerumuthoga@gmail.com>
2023-09-19 17:51:31 -0700

    task 0

--- printf_0.c
+++ printf_0.c
@@ -9,7 +9,7 @@ int _printf(const char *format, ...)
 	unsigned int i, str_count, count = 0;
 	va_list args;
 
-	if (format == NULL)
+	if (!format|| (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 	va_start(args, format);
 
