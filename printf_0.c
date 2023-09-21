16c1069a8833f70287e25435332abfc6a88a4d73
charity-m <njerycharity@gmail.com>
2023-09-21 07:05:27 -0700

    okay we getting there

--- printf_0.c
+++ printf_0.c
@@ -9,39 +9,33 @@ int _printf(const char *format, ...)
 	unsigned int i, str_count, count = 0;
 	va_list args;
 
-	if (!format || (format[0] == '%' && format[1] == '\0'))
+	if (format == NULL)
 		return (-1);
 	va_start(args, format);
 
-	for (i = 0; format[i] != '\0'; i++)
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
 		{
-			_putchar(format[i]);
+			ptcha(format[i]);
 		}
 		else if (format[i + 1] == 'c')
 		{
-			_putchar(va_arg(args, int));
+			ptcha(va_arg(args, int));
 			i++;
 		}
 		else if (format[i + 1] == 's')
 		{
-			str_count = _puts(va_arg(args, char*));
+			str_count = putss(va_arg(args, char*));
 			i++;
 			count += (str_count - 1);
 		}
 		else if (format[i + 1] == '%')
 		{
-			_putchar('%');
-		}
-		else
-		{
-			_putchar('%');
-			i++;
+			ptcha('%');
 		}
 		count += 1;
-	}
-
-	va_end(args);
-	return (count);
+		}
+		va_end(args);
+		return (count);
 }
