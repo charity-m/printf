c38ef334ac836c63c46e7d45f7a906e351fc25ed
charity-m <njerycharity@gmail.com>
2023-09-21 07:09:36 -0700

    task 0

--- printf_0.c
+++ printf_0.c
@@ -9,11 +9,11 @@ int _printf(const char *format, ...)
 	unsigned int i, str_count, count = 0;
 	va_list args;
 
-	if (format == NULL)
+	if (!format || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 	va_start(args, format);
 
-	for (i = 0; format && format[i] != '\0'; i++)
+	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
 		{
