d8ca7ac1c0dc9fd15a87931e69018c392cd436ef
Waerumuthoga <waerumuthoga@gmail.com>
2023-09-22 08:24:00 -0700

    just making sure we are on the same page

--- printf_0.c
+++ printf_0.c
@@ -6,36 +6,17 @@
  */
 int _printf(const char *format, ...)
 {
-	unsigned int i, str_count, count = 0;
+	unsigned int i, str_length, length = 0;
 	va_list args;
 
-	if (!format || (format[0] == '%' && format[1] == '\0'))
+	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
-	va_start(args, format);
+	if (format[0] == '%' && format[1] == ' ')
+		return (-1);
 
+	va_start(args, format);
 	for (i = 0; format[i] != '\0'; i++)
 	{
-		if (format[i] != '%')
+		if (*format == '%' && *(format + 1) != '\0')
 		{
-			ptcha(format[i]);
-		}
-		else if (format[i + 1] == 'c')
-		{
-			ptcha(va_arg(args, int));
-			i++;
-		}
-		else if (format[i + 1] == 's')
-		{
-			str_count = putss(va_arg(args, char*));
-			i++;
-			count += (str_count - 1);
-		}
-		else if (format[i + 1] == '%')
-		{
-			ptcha('%');
-		}
-		count += 1;
-		}
-		va_end(args);
-		return (count);
-}
+
